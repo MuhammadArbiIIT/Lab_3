3bba3724dafe0a046aa63997c7a10c2c642acfa5
MuhammadArbiIIT <marbi@hawk.iit.edu>
2023-10-29 21:57:04 -0500

    add function update field to make tip slider adaptive

--- script.js
+++ script.js
@@ -1,39 +1,38 @@
-//getting id's from HTML 
 document.addEventListener('DOMContentLoaded', function() {
     const textInput = document.getElementById("textInput"); 
     const rangeInput = document.getElementById("rangeInput"); 
     const tip_percent = document.getElementById("tip_percent"); 
     const tip_amount = document.getElementById("tip_amount"); 
     const total_bill = document.getElementById("total_bill"); 
-    const form = document.querySelector(".label") 
-    //listening for input text and rangeInput
+    const form = document.querySelector(".label");
+
+    // Function to update fields
+    function updateFields(billTotalValue, tipSliderValue) {
+        tip_percent.textContent = tipSliderValue; 
+
+        // Math to find out tip amount value and total bill value
+        const tip_amountValue = (billTotalValue * tipSliderValue) / 100; 
+        const totalBillValue = billTotalValue + tip_amountValue; 
+
+        tip_amount.value = tip_amountValue.toFixed(2); 
+        total_bill.value = totalBillValue.toFixed(2);
+    }
+
+    // Listening for input text changes
     if (textInput) {
         textInput.addEventListener("input", function(event) {
             const billTotalValue = parseFloat(event.target.value); 
             const tipSliderValue = parseFloat(rangeInput.value); 
-            tip_percent.textContent = tipSliderValue; 
-
-            //math to find out tip amount value and total bill value
-            const tip_amountValue = (billTotalValue * tipSliderValue) / 100; 
-            const totalBillValue = billTotalValue + tip_amountValue; 
-
-            tip_amount.value = tip_amount.toFixed(2); 
-            total_bill.value = totalBillValue.toFixed(2);
-            
+            updateFields(billTotalValue, tipSliderValue);
         });
     }
-//use rangeInput(tip slider) and display the value as well as use it for calculations
+
+    // Listening for slider changes
     if (rangeInput) {
         rangeInput.addEventListener("input", function(event){
             const tipSliderValue = parseFloat(event.target.value);
-            tip_percent.textContent = tipSliderValue;
-
             const billTotalValue = parseFloat(textInput.value);
-            const tipAmountValue = (billTotalValue * tipSliderValue) / 100; 
-            const totalBillValue = billTotalValue + tipAmountValue; 
-
-            tip_amount.value = tipAmountValue.toFixed(2);
-            total_bill.value = totalBillValue.toFixed(2);
+            updateFields(billTotalValue, tipSliderValue);
         });
     }
 });
